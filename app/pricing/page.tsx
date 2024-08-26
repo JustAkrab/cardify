0514eed758700faf4d6c387e794410a68fc19544
colspan <collin@collindigm.com>
2024-08-26 16:49:54 +0300

    Pricing page initialized

---
+++ app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import {Pricing} from "@/components/ui/pricing";
+import Navbar from "@/components/Navbar";
+
+const PricingPage = () => {
+    return  (
+        <>
+            <Navbar/>
+            <Pricing/>
+        </>
+    )
+}
+
+export default PricingPage;
